dead73c775a0d9dec18527a2ec091194f54c2960
RubenCruchaga <rc103008@wgcloud.org>
2024-04-03 11:39:30 -0500

    fixed agen

--- djangotutorials/tutorialapp/urls.py
+++ djangotutorials/tutorialapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('base/', views.base, name='base')
+    path('base/', views.base, name='base'),
     path('', views.home, name='home')
 ]
