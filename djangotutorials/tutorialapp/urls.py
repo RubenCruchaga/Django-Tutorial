dff8b0e4850e5e65b9e3f4fe2148bb151ce93faf
RubenCruchaga <rc103008@wgcloud.org>
2024-04-03 11:37:45 -0500

    added home urls and templets

--- djangotutorials/tutorialapp/urls.py
+++ djangotutorials/tutorialapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, imclude
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('base/', views.base, name='base')
+    path('', view.home, name='home')
 ]
