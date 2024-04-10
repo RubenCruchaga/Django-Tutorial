e6792bbfb223b20207cf8b7c3006ae14a60f803b
RubenCruchaga <rc103008@wgcloud.org>
2024-04-10 10:08:33 -0500

    added student modle

--- djangotutorials/tutorialapp/models.py
+++ djangotutorials/tutorialapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    Grade =(
+        ('9','9'),
+        ('10','10'),
+        ('11','11'),
+        ('12','12'),
+    )
+
+    firstname = models.CharField(max_Length=200, null=True)
+    lastname = models.CharField(max_Length=200, null=True)
+    middlename = models.CharField(max_Length=200, null=True)
+    grade = models.CharField(max_Length=200, null=True, choices=Grade)
+
+    def __str__(self) -> str:
+        return ""+ str(self.lastname) + ", " + str(self.firstname)
