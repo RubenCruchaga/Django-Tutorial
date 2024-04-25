2ba9ae142197d2e7431fe4b63fa144eae28b151b
RubenCruchaga <rc103008@wgcloud.org>
2024-04-25 11:09:48 -0500

    hosted images on website

--- djangotutorials/djangotutorials/urls.py
+++ djangotutorials/djangotutorials/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 from tutorialapp.views import *
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tutorialapp.urls'))
